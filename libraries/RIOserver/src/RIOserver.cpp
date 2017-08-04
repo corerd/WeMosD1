b71a1f33c8fe67a3c924e344d7f0b1d7398cdef7
Corrado Ubezio <corerd@live.com>
2017-08-04 06:08:39 +0200

    First commit

---
+++ libraries/RIOserver/src/RIOserver.cpp
@@ -0,0 +1,121 @@
+/**
+ * ESP8266 WeMos D1 mini library collection
+ * Remote GPIO (the server part)
+ *
+ * Allow GPIO remote management.
+ *
+ * =============================================================================
+ * This code is placed under the MIT license
+ * Copyright (c) 2016 Corrado Ubezio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =============================================================================
+ */
+
+#include "RIOserver.h"
+
+/// PIN types
+#define PIN_ANALOG	'A'
+#define PIN_DIGITAL	'D'
+#define PIN_VIRTUAL	'V'
+
+/// Received control characters
+#define COMMAND_SEP		'#'
+#define COMMAND_STOPPER	'*'
+
+enum Status_rx {WAIT_PIN_TYPE, WAIT_PIN_ADDRESS, WAIT_SEPARATOR, WAIT_VALUE, WAIT_STOPPER};
+
+
+RIOserver::RIOserver(BluetoothSerial &channel) :
+						m_channel(channel),
+						m_run_status(WAIT_PIN_TYPE)
+{
+	// class constructor
+}
+
+RIOserver::~RIOserver()
+{
+	// class destructor here
+}
+
+/**
+ * Process incoming commands and perform connection housekeeping.
+ *
+ * Command format:
+ * <command> ::= <pin_type><pin_address><separator><pin_value><stopper>
+ * <pin_type> ::= 'A' | 'D' | 'V'
+ * <pin_address> ::= <integer>
+ * <separator> ::= '#'
+ * <pin_value> ::= <integer>
+ * <stopper> ::= '*'
+ * <integer> ::= <digit> | <integer><digit>
+ */
+void RIOserver::run()
+{
+	while (m_channel.available())
+	{
+		switch(m_run_status)
+		{
+			case WAIT_PIN_TYPE:
+				m_pin_type = m_channel.read();
+				switch (m_pin_type) {
+					case PIN_ANALOG:
+					case PIN_DIGITAL:
+					case PIN_VIRTUAL:
+						m_run_status = WAIT_PIN_ADDRESS;
+						break;
+				}
+				break;
+			case WAIT_PIN_ADDRESS:
+				m_pin_address = m_channel.parseInt();
+				m_run_status = WAIT_SEPARATOR;
+				break;
+			case WAIT_SEPARATOR:
+				if (m_channel.read() == COMMAND_SEP)
+				{
+					m_run_status = WAIT_VALUE;
+				}
+				else
+				{
+					m_run_status = WAIT_PIN_TYPE;
+				}
+				break;
+			case WAIT_VALUE:
+				m_pin_value = m_channel.parseInt();
+				m_run_status = WAIT_STOPPER;
+				break;
+			case WAIT_STOPPER:
+				if (m_channel.read() == COMMAND_STOPPER)
+				{
+					exec_command();
+				}
+				m_run_status = WAIT_PIN_TYPE;
+				break;
+		}
+	}
+}
+
+void RIOserver::exec_command()
+{
+	Serial.println("");
+	Serial.println(m_pin_type);
+	Serial.println(m_pin_address);
+	Serial.println(m_pin_value);
+	Serial.println("");
+}
---
+++ libraries/RIOserver/src/RIOserver.h
@@ -0,0 +1,59 @@
+/**
+ * ESP8266 WeMos D1 mini library collection
+ * Remote GPIO (the server part)
+ *
+ * Allow GPIO remote management.
+ *
+ * =============================================================================
+ * This code is placed under the MIT license
+ * Copyright (c) 2016 Corrado Ubezio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =============================================================================
+ */
+
+#ifndef _RIOSERVER_
+#define _RIOSERVER_
+
+#include "Arduino.h"
+#include "BluetoothSerial.h"
+
+
+class RIOserver
+{
+  public:
+    RIOserver(BluetoothSerial &channel);
+    ~RIOserver();
+
+    /// Process incoming commands and perform connection housekeeping
+    void run();
+
+  private:
+    void exec_command();
+
+    // Member variables
+	BluetoothSerial &m_channel;
+    char m_run_status;
+    char m_pin_type;
+    int m_pin_address;
+    int m_pin_value;
+};
+
+
+#endif /* _RIOSERVER_ */
